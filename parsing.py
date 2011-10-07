ef45a236768f92802c9304e23845ca855e077fa9
Raphaël Vinot <raphael@vinot.info>
2011-10-07 14:55:38 -0400

    comments++

--- parsing.py
+++ parsing.py
@@ -1,5 +1,6 @@
 import redis
 
+# All elements of the logs
 elts = ["date", "time", "time-taken", "c-ip", "cs-username", "cs-auth-group",
 "x-exception-id", "sc-filter-result", "cs-categories", "cs(Referer)", "sc-status",
 "s-action", "cs-method", "rs(Content-Type)", "cs-uri-scheme", "cs-host",
@@ -7,15 +8,21 @@ elts = ["date", "time", "time-taken", "c-ip", "cs-username", "cs-auth-group",
 "cs(User-Agent)", "s-ip", "sc-bytes", "cs-bytes", "x-virus-id"]
 
 # OBSERVED == not forbidden
-
 skip_filter_result = ["OBSERVED"]
+
+# Useless (?) elements
 skip_elements = ["date", "time", "time-taken", "c-ip", "cs-categories", "s-ip",\
         "sc-bytes", "cs-bytes"]
 
 import re
+# Ugly regex to parse a line
 p = re.compile(r'(\S+) (\S+) (\d+) (\S+) (\S+) (\S+) (\S+) (\S+) "(\S+)" (\S+)  (\d+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) "(.*)" (\S+) (\d+) (\d+) (\S+)').findall
 
 def stats_elements(splitted_line, pipeline):
+    """
+        Compute occurrences of each values for each elements
+        Except the elemets listed in skip_elements.
+    """
     i = -1
     for s in splitted_line:
         i += 1
@@ -24,16 +31,25 @@ def stats_elements(splitted_line, pipeline):
         pipeline.zincrby(elts[i], s)
 
 def stats_full_url(splitted_line, pipeline):
+    """
+        Rebuild an URL and compute occurrences
+    """
     full_url = splitted_line[14] + "://" + splitted_line[15] + ":" + splitted_line[16] + splitted_line[17]
     pipeline.zincrby("full_url", full_url)
 
 def stats_words(splitted_line, pipeline):
+    """
+        Compute occurrences of each words
+    """
     words = re.findall(r"\w+", splitted_line[17])
     for w in words:
         if len(w) > 3 :
             pipeline.zincrby("words", w)
 
 def parser(line, pipeline):
+    """
+        Main function.
+    """
     splitted = p(line)
     i = 0
     if splitted is not None and len(splitted) != 0:
